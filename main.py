d96a4918681a318ab4d853ad863ac88957444354
elisakaisa <elisa.k.perini@gmail.com>
2023-01-27 16:56:58 +0100

    improved chart layout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import pandas as pd
 from scipy import signal
@@ -51,85 +52,96 @@ df["left wrist x (SG w10 p4)"] = df["left wrist x (SG w10 p4)"].shift(34)
 df["left wrist y (SG w10 p4)"] = df["left wrist y (SG w10 p4)"].shift(34)
 
 # Plots for visualizing the data
-fig1, axes = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
+fig1, axes = plt.subplots(nrows=2, ncols=3, figsize=(13, 12))
+fig1.suptitle("Left wrist semi-circle movement filtered with different filters", fontsize=20)
 axes = np.reshape(axes, -1)
 
 axes[0].plot(df['left wrist x raw'], df['left wrist y raw'])
-axes[0].set(title="Left wrist movement, raw data", xlabel="x",ylabel="y")
+axes[0].set(title="a) raw data", xlabel="x",ylabel="y")
 axes[0].legend()
 
 axes[1].plot(df['left wrist x (3)'], df['left wrist y (3)'])
-axes[1].set(title="Left wrist movement, moving avg, window 3", xlabel="x",ylabel="y")
+axes[1].set(title="b) moving avg, window 3", xlabel="x",ylabel="y")
 axes[1].legend()
 
 axes[2].plot(df['left wrist x (10)'], df['left wrist y (10)'])
-axes[2].set(title="Left wrist movement, moving avg, window 10", xlabel="x",ylabel="y")
+axes[2].set(title="c) moving avg, window 10", xlabel="x",ylabel="y")
 axes[2].legend()
 
 axes[3].plot(df['left wrist x (30)'], df['left wrist y (30)'])
-axes[3].set(title="Left wrist movement, moving avg, window 30", xlabel="x",ylabel="y")
+axes[3].set(title="d) moving avg, window 30", xlabel="x",ylabel="y")
 axes[3].legend()
 
 axes[4].plot(df['left wrist x (SG w4 p3)'], df['left wrist y (SG w4 p3)'])
-axes[4].set(title="Left wrist movement, SA, window 4, polyorder 3", xlabel="x",ylabel="y")
+axes[4].set(title="e) SG, window 4, polyorder 3", xlabel="x",ylabel="y")
 axes[4].legend()
 
 axes[5].plot(df['left wrist x (SG w10 p4)'], df['left wrist y (SG w10 p4)'])
-axes[5].set(title="Left wrist movement, SA, window 10, polyorder 4", xlabel="x",ylabel="y")
+axes[5].set(title="f) SG, window 10, polyorder 4", xlabel="x",ylabel="y")
 axes[5].legend()
 
 
-fig2, axes = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
+# Create a Rectangle patch
+x_lower_lim = 260
+y_lower_lim = 300
+height = 100
+width = 80
+rect1 = patches.Rectangle((x_lower_lim, y_lower_lim), width, height, linewidth=1, edgecolor='k', facecolor='none')
+rect2 = patches.Rectangle((x_lower_lim, y_lower_lim), width, height, linewidth=1, edgecolor='k', facecolor='none')
+
+
+fig2, axes = plt.subplots(nrows=3, ncols=2, figsize=(12, 15))
+fig2.suptitle("Comparison of different filters on left wrist semi-circle movement", fontsize=20)
 axes = np.reshape(axes, -1)
 
 axes[0].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw data")
 axes[0].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA, window of 3")
 axes[0].plot(df['left wrist x (10)'], df['left wrist y (10)'], label="MA, window of 10")
 axes[0].plot(df['left wrist x (30)'], df['left wrist y (30)'], label="MA, window of 30")
-axes[0].set(title="Left wrist movement", xlabel="x",ylabel="y")
+axes[0].set(title="a) Moving average filters", xlabel="x",ylabel="y")
+axes[0].add_patch(rect1)
 axes[0].legend()
 
-axes[1].plot(df['time'], df['left wrist x raw'], label="raw data")
-axes[1].plot(df['time'], df['left wrist x (3)'], label="MA, window of 3")
-axes[1].plot(df['time'], df['left wrist x (10)'], label="MA, window of 10")
-axes[1].plot(df['time'], df['left wrist x (30)'], label="MA, window of 30")
-axes[1].set(title="Left wrist movement", xlabel="time [s]",ylabel="x coordinate")
+axes[1].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw")
+axes[1].plot(df['left wrist x (SG w4 p3)'], df['left wrist y (SG w4 p3)'], label="SG (window 4, polyorder 3)")
+axes[1].plot(df['left wrist x (SG w10 p4)'], df['left wrist y (SG w10 p4)'], label="SG (window 10, polyorder 4)")
+axes[1].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA (window 3)")
+axes[1].set(title="c) Savintzky-Golay filters and moving avg with window 3", xlabel="x",ylabel="y")
+axes[1].add_patch(rect2)
 axes[1].legend()
 
-axes[2].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw")
-axes[2].plot(df['left wrist x (SG w4 p3)'], df['left wrist y (SG w4 p3)'], label="SG (window 4, polyorder 3")
-axes[2].plot(df['left wrist x (SG w10 p4)'], df['left wrist y (SG w10 p4)'], label="SG (window 10, polyorder 4")
-axes[2].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA (window 3")
-axes[2].set(title="Left wrist movement, SG, window 4, polyorder 3", xlabel="x",ylabel="y")
+axes[2].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw data")
+axes[2].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA, window of 3")
+axes[2].plot(df['left wrist x (10)'], df['left wrist y (10)'], label="MA, window of 10")
+axes[2].plot(df['left wrist x (30)'], df['left wrist y (30)'], label="MA, window of 30")
+axes[2].set(title="d) Zoomed in moving average filters", xlabel="x",ylabel="y")
+axes[2].set_ylim(y_lower_lim, y_lower_lim + height)
+axes[2].set_xlim(x_lower_lim, x_lower_lim + width)
 axes[2].legend()
 
-axes[3].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw data")
-axes[3].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA, window of 3")
-axes[3].plot(df['left wrist x (10)'], df['left wrist y (10)'], label="MA, window of 10")
-axes[3].plot(df['left wrist x (30)'], df['left wrist y (30)'], label="MA, window of 30")
-axes[3].set(title="Left wrist movement", xlabel="x",ylabel="y")
-axes[3].set_ylim(300, 400)
-axes[3].set_xlim(250, 360)
+axes[3].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw")
+axes[3].plot(df['left wrist x (SG w4 p3)'], df['left wrist y (SG w4 p3)'], label="SG (window 4, polyorder 3")
+axes[3].plot(df['left wrist x (SG w10 p4)'], df['left wrist y (SG w10 p4)'], label="SG (window 10, polyorder 4")
+axes[3].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA (window 3)")
+axes[3].set(title="f) Zoomed in on Savintzky-Golay filters", xlabel="x",ylabel="y")
+axes[3].set_ylim(y_lower_lim, y_lower_lim + height)
+axes[3].set_xlim(x_lower_lim, x_lower_lim + width)
 axes[3].legend()
 
 axes[4].plot(df['time'], df['left wrist x raw'], label="raw data")
-axes[4].plot(df['time'], df['left wrist x (3)'], label="MA, window of 3")
-axes[4].plot(df['time'], df['left wrist x (10)'], label="MA, window of 10")
-axes[4].plot(df['time'], df['left wrist x (SG w4 p3)'], label="SG, window 4, polyorder 3")
-axes[4].plot(df['time'], df['left wrist x (SG w10 p4)'], label="SG, window 10, polyorder 4")
-axes[4].set(title="Left wrist movement", xlabel="time [s]",ylabel="x coordinate")
-axes[4].legend()
+axes[4].plot(df['time'], df['left wrist x (3)'], label="MA, w 3")
+axes[4].plot(df['time'], df['left wrist x (10)'], label="MA, w 10")
+axes[4].plot(df['time'], df['left wrist x (30)'], label="MA, w 30")
+axes[4].set(title="b) Moving average filter over time", xlabel="time [s]",ylabel="x coordinate")
+axes[4].legend(loc="center left")
 
-axes[5].plot(df['left wrist x raw'], df['left wrist y raw'], label="raw")
-axes[5].plot(df['left wrist x (SG w4 p3)'], df['left wrist y (SG w4 p3)'], label="SG (window 4, polyorder 3")
-axes[5].plot(df['left wrist x (SG w10 p4)'], df['left wrist y (SG w10 p4)'], label="SG (window 10, polyorder 4")
-axes[5].plot(df['left wrist x (3)'], df['left wrist y (3)'], label="MA (window 3)")
-axes[5].set(title="Left wrist movement, SG, window 4, polyorder 3", xlabel="x",ylabel="y")
-axes[5].set_ylim(300, 400)
-axes[5].set_xlim(250, 360)
-axes[5].legend()
-
-
+axes[5].plot(df['time'], df['left wrist x raw'], label="raw data")
+axes[5].plot(df['time'], df['left wrist x (3)'], label="MA, window of 3")
+axes[5].plot(df['time'], df['left wrist x (10)'], label="MA, window of 10")
+axes[5].plot(df['time'], df['left wrist x (SG w4 p3)'], label="SG, w 4, p 3")
+axes[5].plot(df['time'], df['left wrist x (SG w10 p4)'], label="SG, w 10, p 4")
+axes[5].set(title="e) Savintzky-Golay over time", xlabel="time [s]",ylabel="x coordinate")
+axes[5].legend(loc="center left")
 
 plt.show()
 
